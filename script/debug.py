9101b274627e0b4ddc3960d083664c1360305e2c
Left <fuerdi2@163.com>
2018-03-31 14:56:51 +0800

    更改

--- script/debug.py
+++ script/debug.py
@@ -3,13 +3,17 @@ import pandas as pd
 import numpy as np
 import webroute as sh
 import time
-f = os.path.dirname(os.getcwd())+'/data/major_edges_lnglat.csv'
-df = pd.read_csv(f)
-df = df[(df['oneway']) & (df['length']>100)]
-interval = 0.1
-threshold = 60
-r = {} # store the result
+import os
+
+f = os.path.dirname(os.getcwd())+'/data/137平方公里城区道路信息表火星坐标系.xlsx'
+df = pd.read_excel(f)
+interval = 0.01
+threshold = 80
 for i in df.index:
     time.sleep(interval)
-    OBJECTID,f_lat,f_lng,t_lat,t_lng,road_length = df.loc[i,'OBJECTID'],df.loc[i,'from_lat_mars'],df.loc[i,'from_lng_mars'],df.loc[i,'to_lat_mars'],df.loc[i,'to_lng_mars'],df.loc[i,'length']
-    r[OBJECTID]=sh.isShortest(f_lng,f_lat,t_lng,t_lat,road_length,threshold)
+    try:
+    	OBJECTID,f_lat,f_lng,t_lat,t_lng,road_length = i,df.loc[i,'from_lat_mars'],df.loc[i,'from_lng_mars'],df.loc[i,'to_lat_mars'],df.loc[i,'to_lng_mars'],df.loc[i,'length']
+    	df.loc[i,'isShortest']=sh.isShortest(f_lng,f_lat,t_lng,t_lat,road_length,threshold)
+    except KeyError:
+     	pass
+df.to_excel('校核结果.excel')
--- script/webroute.py
+++ script/webroute.py
@@ -2,17 +2,19 @@
 import time
 import pandas as pd
 from urllib.request import urlopen
+from urllib import error
 from bs4 import BeautifulSoup
 url = 'http://restapi.amap.com/v3/direction/driving?'; # this is the webapi of Amap
 key = "2609ba44c963af81305d291908f8f293"; #this is yourKey
+key_qiye = 'a44238fae301d7143a9e40ae5f01d54d'
 stg = 2; #the shortest path driving strategy
 def isShortest(f_lng,f_lat,t_lng,t_lat,length,threshold):
 	URL = url+"origin="+str(f_lng)+','+str(f_lat)+"&destination="+str(t_lng)+','+str(t_lat)+ \
-	"&strategy="+str(stg)+"&extensions=all&output=xml&key="+key;
-	try :
+	"&strategy="+str(stg)+"&extensions=all&output=xml&key="+key_qiye;
+	try:
 	    html = urlopen(URL);
-	except HTTPError as e:
-	    print("URL error");
+	except error.URLError as e:
+	    print(e.reason);
 	bsObj =  BeautifulSoup(html.read().decode("utf-8"));
 	try:
 		routes = len(bsObj.steps);
@@ -21,16 +23,33 @@ def isShortest(f_lng,f_lat,t_lng,t_lat,length,threshold):
 	distance = float(bsObj.route.path.distance.getText());
 	if(routes==0):
 	    print("no steps");
-	return (routes==1 or abs(distance-length)<threshold);
+	return 1 if(routes==1 or abs(distance-length)<threshold) else 0;
 def getStatus(f_lng,f_lat,t_lng,t_lat):
 	URL = url+"origin="+str(f_lng)+','+str(f_lat)+"&destination="+str(t_lng)+','+str(t_lat)+ \
-	"&strategy="+str(stg)+"&extensions=all&output=xml&key="+key;
+	"&strategy="+str(stg)+"&extensions=all&output=xml&key="+key_qiye;
 	try :
-		html = urlopen(URL);
-	except HTTPError as e:
-		print("URL error");
-	bsObj =  BeautifulSoup(html.read().decode("utf-8"));
-	distance = float(bsObj.route.path.distance.getText())
-	time = float(bsObj.route.path.duration.getText())
-	status = bsObj.route.path.tmc.status.getText()
+		html = urlopen(URL,timeout = 60);
+	except error.URLError as e:
+		print(e.reason);
+	try:
+		bsObj =  BeautifulSoup(html.read().decode("utf-8"));
+		distance = float(bsObj.route.path.distance.getText())
+		time = float(bsObj.route.path.duration.getText())
+		status = bsObj.route.path.tmc.status.getText()
+	except AttributeError:
+		pass
 	return distance,time,status
+def getWeather():
+	URL = 'http://restapi.amap.com/v3/weather/weatherInfo?key='+key+'&city=320200&output=xml'
+	try :
+		html = urlopen(URL,timeout = 60)
+	except error.URLError as e:
+		print(e.reason);
+	try:
+		bsObj =  BeautifulSoup(html.read().decode("utf-8"))
+		tempe = float(bsObj.lives.temperature.getText())
+		humid = float(bsObj.lives.humidity.getText())
+		weather = str(bsObj.lives.weather.getText())
+	except AttributeError:
+		pass
+	return tempe,humid,weather
